a4dc19dd4ab59d486dd90652b185a7d0f91809e2
nataniel44 <josenatanielsoto@gmail.com>
2024-11-28 21:59:21 -0300

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// next.config.js
+module.exports = {
+    webpack(config) {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            '@': path.resolve(__dirname),  // Asumiendo que '@' se refiere al directorio raíz del proyecto
+        };
+        return config;
+    },
+};
