83f3a231cf2650c04b26eb96c6b4d7310a4c0a5e
Nataniel44 <josenatanielsoto@gmail.com>
2025-10-30 21:41:31 -0300

    unnuevocomienzo

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,5 +1,10 @@
 import { RestaurantMenu } from "@/components/RestaurantMenu";
 
-export default function Page({ params }: { params: any }) {
-    return <RestaurantMenu slug={params?.slug} />;
+export default async function Page({
+    params,
+}: {
+    params: Promise<{ slug: string }>;
+}) {
+    const { slug } = await params;
+    return <RestaurantMenu slug={slug} />;
 }
