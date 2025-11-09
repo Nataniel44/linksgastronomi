677804645302adc468c7403b159a735041e7963c
Nataniel44 <josenatanielsoto@gmail.com>
2025-11-09 13:04:58 -0300

    varios-cambios-ej-immagenes

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -8,8 +8,10 @@ export default async function RestaurantPage({
     const { slug } = await params;
 
     const baseUrl =
+
         process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
+        
     const res = await fetch(`${baseUrl}/api/menu/${slug}`, {
         cache: "no-store",
     });
